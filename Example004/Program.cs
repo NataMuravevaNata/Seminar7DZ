397a33c5a91482f7a2450c4a50e05ec5d64ac116
Nata <muravevantsh@mail.ru>
2022-09-02 00:44:15 +0300

    Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,77 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой 
+//строки двумерного массива
+void PrintMatrix (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j <matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillMatrix (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j =0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    } 
+}
+
+//void SelectionSort (int [,] matr)
+//{
+//    for (int i = 0; i < matr.GetLength(0); i++)
+//        {
+ //           for (int j =0; j < matr.GetLength(1); j++)
+ //       
+ //               {
+ //               
+//int maxPosition = i;
+ //               for (int k = i+1; k < matr.GetLength(0); k++)
+ //                  {
+ //                        if ( matr[k,j] > matr[maxPosition, j]) maxPosition=k;
+//                     }    
+ //               int temporary = matr[i,j];
+//                 matr[i,j]=matr[maxPosition, j];
+ //                matr[maxPosition, j]=temporary; 
+//                 }
+//
+//        }
+//}
+void SelectionSort (int [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            for (int j =0; j <matr.GetLength(1); j++)
+        
+                {
+                
+int maxPosition = j;
+                for (int k = j+1; k <= matr.GetLength(0); k++)
+                   {
+                         if ( matr[i,k] > matr[i,maxPosition]) maxPosition=k;
+                     }    
+                int temporary = matr[i,j];
+                 matr[i,j]=matr[i,maxPosition];
+                 matr[i,maxPosition]=temporary; 
+                 }
+
+        }
+}
+                    
+                                             
+int m = 5;
+int n = 6;
+int[,]matrix = new int [m,n];
+PrintMatrix(matrix);
+FillMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+SelectionSort(matrix);
+PrintMatrix(matrix);
